ec0e66a7e2361d44126c802805817f4b1f1d2694
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2023-09-11 18:00:17 -0300

    Finalizando as functions

--- functions.sql
+++ functions.sql
@@ -1,36 +1,33 @@
-CREATE OR REPLACE FUNCTION FNC_QTD_ANALISES(
+-- Funcaoo para retornar o porcentual de analises com o valor do parametro passado
+-- referente ao total de analises.
+
+CREATE OR REPLACE FUNCTION FNC_PORCENT_ANALISES(
     p_status IN VARCHAR
 )
 RETURN NUMBER
 IS
 
-    resultado NUMBER;
-    id_status NUMBER;
+    v_total NUMBER;
+    v_porStatus NUMBER;
+    v_resultado NUMBER;
 
 BEGIN
 
     IF UPPER(p_status) IN ('MORTA', 'EM PROGRESSO', 'EM TRATAMENTO', 'ANALISANDO', 'CURADA') THEN
     
-        SELECT ID_STATUS
-        INTO id_status
-        FROM TB_AS_STATUS
-        WHERE UPPER(DS_STATUS) = UPPER(p_status);
-        
-        DBMS_OUTPUT.PUT_LINE('ID do status: ' || id_status);
-        DBMS_OUTPUT.PUT_LINE('Quantidade: ' || resultado);
-        
         SELECT COUNT(*)
-        INTO resultado
-        FROM TB_AS_ANALISE
-        WHERE ID_STATUS = TO_NUMBER(id_status);
+        INTO v_total
+        FROM TB_AS_ANALISE;
+    
+        SELECT COUNT(*)
+        INTO v_porStatus
+        FROM TB_AS_ANALISE a 
+        INNER JOIN TB_AS_STATUS s ON (a.id_status = s.id_status)
+        WHERE UPPER(s.DS_STATUS) = UPPER(p_status);
         
-        DBMS_OUTPUT.PUT_LINE('Quantidade: ' || resultado);
+        v_resultado := (v_porStatus / v_total) * 100;
 
-        IF resultado IS NULL THEN
-           RAISE_APPLICATION_ERROR(-20003, 'Nenhuma análise encontrada para o valor: ' || p_status);
-        ELSE
-            RETURN resultado;
-        END IF;
+        RETURN v_resultado;
         
     ELSE
     
@@ -40,22 +37,37 @@ BEGIN
         
 EXCEPTION
 
-  WHEN OTHERS THEN
-    DBMS_OUTPUT.PUT_LINE('Ocorreu uma exceção: ' || SQLERRM);
-    RETURN NULL;
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nenhuma solucao encontrada para o problema: ' || p_status);
+        RETURN 0;
+
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Ocorreu uma execucao: ' || SQLERRM);
+        RETURN NULL;
     
 END;
+
+-- Demonstracao
 
 DECLARE
 
     v_resultado NUMBER;
+    v_status VARCHAR2(50) := 'ANALISANDO';
 
 BEGIN
 
-    v_resultado := FNC_QTD_ANALISES('ANALISANDO');
-    DBMS_OUTPUT.PUT_LINE(v_resultado);
+    v_resultado := FNC_PORCENT_ANALISES(v_status);
+    DBMS_OUTPUT.PUT_LINE('A porcentagem de analises com o status ' || v_status || ' e: ' || v_resultado);
 
 END;
+
+
+
+
+
+
+-- Funcao para retornar uma solucação para o problema passado como parametro com base em palavra ou pequenas 
+-- frases chaves.
 
 CREATE OR REPLACE FUNCTION FNC_RETORNA_SOLUCAO(
     p_problema IN VARCHAR2
@@ -70,21 +82,25 @@ BEGIN
     SELECT DS_SOLUCAO
     INTO v_retorno
     FROM TB_AS_ANALISE
-    WHERE UPPER(DS_PROBLEMA) LIKE '%' || UPPER(p_problema) || '%';
+    WHERE UPPER(DS_PROBLEMA) LIKE '%' || UPPER(p_problema) || '%'
+    AND DT_REGISTRO = (SELECT MAX(DT_REGISTRO) FROM TB_AS_ANALISE)
+    AND ROWNUM = 1;
     
     RETURN v_retorno;
     
 EXCEPTION
 
     WHEN NO_DATA_FOUND THEN
-      DBMS_OUTPUT.PUT_LINE('Nenhuma solução encontrada para o problema: ' || p_problema);
-      RETURN NULL;
+        DBMS_OUTPUT.PUT_LINE('Nenhuma solucao encontrada para o problema: ' || p_problema);
+        RETURN NULL;
 
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Ocorreu uma exceção: ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('Ocorreu uma execucao: ' || SQLERRM);
         RETURN NULL;
 
 END;
+
+-- Demonstração
 
 DECLARE
 
@@ -92,7 +108,7 @@ DECLARE
 
 BEGIN
 
-    v_resultado := FNC_RETORNA_SOLUCAO('banana');
+    v_resultado := FNC_RETORNA_SOLUCAO('folha');
     DBMS_OUTPUT.PUT_LINE(v_resultado);
 
 END;
@@ -100,18 +116,6 @@ END;
 
 
 
-SELECT * FROM TB_AS_STATUS;
-SELECT * FROM TB_AS_ANALISE;
-desc TB_AS_ANALISE;
-
-SELECT COUNT(*)
-FROM TB_AS_ANALISE
-WHERE ID_STATUS = 4;
-
-SELECT DS_SOLUCAO
-FROM TB_AS_ANALISE
-WHERE DS_PROBLEMA LIKE '%' || 'murchas' || '%';
 
 
-SET SERVEROUTPUT ON
 
